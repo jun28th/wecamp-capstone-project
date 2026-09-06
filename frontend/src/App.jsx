717db127b2bb9a45222fd7f0e71d0faa1db7fbcc
jun28th <doanlevy2806@gmail.com>
2026-09-06 19:00:07 +0700

    Revert "fix: frontend init - KAN-4"
    This reverts commit 9e73d66eef34c3d6d55a9456db9132fe31838779.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import AppRouter from './routes/AppRouter'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  return <AppRouter />
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    axios.get('/api')
+      .then(res => setMessage(res.data))
+      .catch(err => console.error('Lỗi:', err))
+  }, [])
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Frontend - Backend response:</h1>
+      <p>{message || 'Đang tải...'}</p>
+    </div>
+  )
 }
 
 export default App
--- frontend/src/api/axiosClient.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    headers: { 'Content-Type': 'application/json' },
-    timeout: 10000,
-})
-  
-export default axiosClient
