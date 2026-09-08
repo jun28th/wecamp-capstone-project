13080f9947a9be2652afa6c50b23a6f8d95ce78b
thaovy3724 <thaovy3724@gmail.com>
2026-09-08 10:43:37 +0700

    fix: apply middleware to router -KAN-20

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import UserController from "../controllers/user.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 const router = express.Router();
-router.post("/", UserController.createUser);
+router.post("/", verifyToken, UserController.createUser);
 export default router;
